c7345d16585e6b237fc78f9237e091eb15fe0053
Cerise Miller <cerisemiller@gmail.com>
2018-07-10 21:02:10 -0500

    initial frontend test

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <container>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          fUsebx
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link active" href="#">
+              About <span class="sr-only">(current)</span>
+            </a>
+            <a className="nav-item nav-link" href="#">
+              Sign In
+            </a>
+          </div>
+        </div>
+      </nav>
+      <div>BODY</div>
+      <nav class="navbar navbar-default navbar-fixed-bottom">
+  <div class="container">
+  Footer
+  </div>
+</nav>
+      </container>
     );
   }
 }
