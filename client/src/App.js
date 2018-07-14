beb53bf04c57420a5394c305f197e4c5862a16c3
Sean Brown <sbbrown35@gmail.com>
2018-07-14 08:59:34 -0500

    database
    mongo - database

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,21 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import LoginForm from './Pages/LoginForm'; 
+//import ProfileForm from './Pages/ProfileForm'
+import "./index.css"; 
 
-class App extends Component {
-  render() {
-    return (
-      <container>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          fUsebx
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-item nav-link active" href="#">
-              About <span class="sr-only">(current)</span>
-            </a>
-            <a className="nav-item nav-link" href="#">
-              Sign In
-            </a>
-          </div>
-        </div>
-      </nav>
-      <div>BODY</div>
-      <nav class="navbar navbar-default navbar-fixed-bottom">
-  <div class="container">
-  Footer
-  </div>
-</nav>
-      </container>
-    );
-  }
-}
+
+const App = () => (
+  <Router>
+    <div>
+
+      <Switch>
+        <Route exact path="/login" component={LoginForm} />
+       
+      </Switch>
+ 
+      </div>
+    </Router>
+)
 
 export default App;
