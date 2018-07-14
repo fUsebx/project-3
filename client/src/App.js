cbfe3d147a27ef3f4c94c50b47d30e0402a50e94
Cerise Miller <cerisemiller@gmail.com>
2018-07-14 10:11:37 -0500

    app.js update

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,9 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import Navbar from "./Components/Navbar";
-import "./App.css";
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar></Navbar>
-          </div>
-    );
-  }
-}
-=======
 import React from "react";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import LoginForm from './Pages/LoginForm'; 
 //import ProfileForm from './Pages/ProfileForm'
 import "./index.css"; 
 
->>>>>>> ea67865b9e92ef5d37b116a76b49fc72153383e9
 
 const App = () => (
   <Router>
