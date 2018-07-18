36cf4e3a9e7b9cc140cd5398ba60bb5902021d14
Cerise Miller <cerisemiller@gmail.com>
2018-07-17 21:40:48 -0500

    front end updates

--- client/src/Components/Navbar/NavBar.js
+++ client/src/Components/Navbar/NavBar.js
@@ -3,35 +3,79 @@ import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 export default () => (
-  <nav className="navbar navbar-static-top">
-    <div className="container nav-container">
-      <a className="logo-btn" active>
-        <Link to="/home">
-        <img
-          className="img img-logo"
-          src={require("./assets/logo.png")}
-          alt="logo"
-        />
-        </Link>
-      </a>
-      <button className="nav-link" active> 
-        <Link className="nav-btn" to="/">Sign Out</Link>
-      </button>
-      <button className="nav-link" active> 
-        <Link className="btn-nav" to="/dashboard">Dashboard</Link>
-      </button>
-      <button className="nav-link" active> 
-        <Link className="btn-nav" to="/profile">Profile</Link>
-      </button>
-      <button className="nav-link" active> 
-        <Link className="btn-nav" to="/create_account">Create Account</Link>
-      </button>
-      <button className="nav-link" active> 
-        <Link className="btn-nav" to="/login">Sign In</Link>
-      </button>
-      <button className="nav-link" active> 
-        <Link className="btn-nav" to="/about">About</Link>
-      </button>
-    </div>
-  </nav>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+  <a className="navbar-brand">
+  <Link to="/home"><img className="img img-logo" src={require("./assets/logo.png")} alt="logo"/></Link></a>
+
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      
+      <li className="nav-item">
+      <Link className="btn-nav" to="/about">About</Link>
+      </li>
+
+      <li className="nav-item">
+      <Link className="btn-nav" to="/login">Sign In</Link>
+      </li>
+
+      <li className="nav-item">
+      <Link className="btn-nav" to="/create_account">Create Account</Link>
+      </li>
+
+      <li className="nav-item">
+      <Link className="btn-nav" to="/profile">Profile</Link>
+      </li>      
+      
+      <li className="nav-item">
+      <Link className="btn-nav" to="/dashboard">Dashboard</Link>
+      </li>      
+      
+      <li className="nav-item">
+      <Link className="btn-nav" to="/login">Sign Out</Link>
+      </li>
+
+    </ul>
+  </div>
+
+</nav>
 );
+
+
+// export default () => (
+//   <nav className="navbar navbar-static-top">
+//     <div className="container nav-container">
+//       <a className="logo-btn" active>
+//         <Link to="/home">
+//         <img
+//           className="img img-logo"
+//           src={require("./assets/logo.png")}
+//           alt="logo"
+//         />
+//         </Link>
+//       </a>
+//       <button className="nav-link" active> 
+//         <Link className="nav-btn" to="/">Sign Out</Link>
+//       </button>
+//       <button className="nav-link" active> 
+//         <Link className="btn-nav" to="/dashboard">Dashboard</Link>
+//       </button>
+//       <button className="nav-link" active> 
+//         <Link className="btn-nav" to="/profile">Profile</Link>
+//       </button>
+//       <button className="nav-link" active> 
+//         <Link className="btn-nav" to="/create_account">Create Account</Link>
+//       </button>
+//       <button className="nav-link" active> 
+//         <Link className="btn-nav" to="/login">Sign In</Link>
+//       </button>
+//       <button className="nav-link" active> 
+//         <Link className="btn-nav" to="/about">About</Link>
+//       </button>
+//     </div>
+//   </nav>
+// );
--- client/src/Pages/Profile/Profile.js
+++ client/src/Pages/Profile/Profile.js
@@ -22,7 +22,7 @@ class Profile extends React.Component {
   render() {
     return (
       <div className="profile-page">
-        >
+      
       </div>
     );
   }
