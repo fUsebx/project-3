d37822f89abb3a7f1254df62650630b42cdf2f56
Sean Brown <sbbrown35@gmail.com>
2018-07-19 18:35:29 -0500

    update
    update

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose'); 
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; 
+
+console.log("Hello"); 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
