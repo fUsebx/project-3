d01ec9992c75277ce81e764c83c5d5dc8c74ff16
Cerise Miller <cerisemiller@gmail.com>
2018-07-26 21:33:27 -0500

    reddit update

--- client/src/Pages/Dashboard/Dashboard.js
+++ client/src/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "./Dashboard.css";
 import DashboardLayout from "../../Components/DashboardLayout";
-<<<<<<< HEAD
 import WeatherAPI from "../WeatherAPI";
-// import RedditScrape from '../RedditScrape';
+import RedditScrape from '../RedditScrape';
 import ToDoList from "../ToDoList";
 // import NewsAPI from '../NewsAPI';
 
@@ -21,7 +20,7 @@ const Dashboard = props => (
           <ToDoList />
         </div>
         <div className="col-md-6">
-            {/* <RedditScrape /> */}
+            <RedditScrape />
         </div>
       </div>
       <br />
@@ -33,22 +32,5 @@ const Dashboard = props => (
     </div>
   </DashboardLayout>
 );
-=======
-import WeatherAPI from '../WeatherAPI'; 
-import RedditScrape from '../RedditScrape'; 
-import ToDoList from '../ToDoList'; 
-// import NewsAPI from '../NewsAPI'; 
-
-const Dashboard = (props) => (
-            <DashboardLayout>
-                <WeatherAPI />
-                <br />
-                <ToDoList />
-                <RedditScrape />
-                {/* <NewsAPI /> */}
-            </DashboardLayout>
-            
-        );
->>>>>>> 9deb7a27f66981686a70f2751af2faa94b633a61
 
 export default Dashboard;
